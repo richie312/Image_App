9f563516fc0511928e6c803c3052012529e31db4
richie312 <richie.chatterjee31@gmail.com>
2018-06-04 08:16:54 +0530

    Added the application files

---
+++ UI.R
@@ -0,0 +1,73 @@
+
+library(shiny)
+library(shinythemes)
+library(keras)
+library(knitr)
+library(devtools)
+library(dplyr)
+library(wordcloud2)
+library(tm)
+library(RColorBrewer)
+library(mgcv)
+library(ggplot2)
+library(markdown)
+
+shinyUI(fluidPage(theme = shinytheme('cerulean'),
+                  
+        navbarPage(  title = "Image Classification with Pre-trained Models",  
+                     id= "nav",
+                     tabPanel("Image", value ="Image",
+    fluidRow(style="background-color:white;",
+      column(6
+             , fluidRow(
+               column(6, br(),
+                      fileInput(inputId = "file",label = "File", buttonLabel="Upload Image",
+                                   accept = c(
+                                     ".jpg",
+                                     ".jpeg",
+                                     ".png")),
+                      helpText("Default max. file size is 10MB"),
+                      
+                      helpText(h5("This Application is preloaded with few images for the detection"))
+                      , div(style = "height:100px;"))
+               , column(6, br(),
+                        selectInput(inputId = "Model", label = "Model", 
+                                       choices = c("resnet50","inception"),
+                                       selected = "inception"),
+                        br(),
+                        helpText(h5("Keras is a user-friendly neural networks API. This application uses the ResNet50 & Inception V3 model, with weights pre-trained on ImageNet.")),
+                        
+                        div(style = "height:100px;"))),
+              fluidRow(
+               column(12,
+                      helpText(h3("Word Cloud for the image object",style="color:#C0392B")),
+                      
+                      column(4,radioButtons(inputId="color", label = "Select color", choices =c("random-light","random-dark"),selected='random-light')),
+                      column(4 ,sliderInput(inputId="Font",label="Font Size",min=0, max=1, value = 0.5)),
+                      wordcloud2Output(outputId="word",width="450px",height="600px")
+                      ,
+                      div(style = "height:600px;"))
+             )
+      )
+      , column(6,
+               helpText(h3("Image Preview",style="color:#C0392B")),
+               selectInput(inputId="Image_class",label="Select Image",choices=c("RaspberryPi.jpg","Cup.jpg","Cat.jpg","Dog.jpg","Bruno(Robot).jpg"),selected="RaspberryPi.jpg"),
+               br(),
+               imageOutput(outputId="image", width="600px", height="350px"),
+               br(),br(),
+               helpText(h3("Bar Plot for the image object.", style="color:#C0392B")),
+               plotOutput(outputId="ggplot"), 
+               
+               div(style = "height:600px;"))
+    )
+  ),
+  tabPanel("Links & Videos",value = "Resources",
+           fluidRow(column(12,style="background-color:#D5F5E3;",
+                           includeMarkdown("resources.rmd")))
+           
+           
+        )
+     )
+  )
+)
+
---
+++ server.R
@@ -0,0 +1,152 @@
+
+
+
+library(shiny)
+library(shinythemes)
+library(keras)
+library(knitr)
+library(devtools)
+library(dplyr)
+library(wordcloud2)
+library(tm)
+library(RColorBrewer)
+library(mgcv)
+library(ggplot2)
+
+
+## Current Working directory
+##setwd("C:/Users/Richie/Desktop/Image_App")
+
+## Download the below pretrained mdoels from the internet
+model_resnet<- application_resnet50(weights = 'imagenet')
+#model_vgg16<-application_vgg16(weights="imagenet", include_top = FALSE)
+model_inception<-application_inception_v3(weights = "imagenet")
+
+## Load the previously saved models
+
+#model_resnet<-load_model_hdf5(file = "model_resnet")
+#model_inception<-load_model_hdf5(file = "model_inception")
+
+
+
+server<-shinyServer(function(input,output){
+  
+  
+
+output$image<-renderImage({
+  par(bg="#F5EEF8")
+  src=input$file
+  src=src$datapath
+  if(is.null(src)){
+    return(list(src=as.character(paste("www/",input$Image_class,sep="")), height="350"))
+  }
+    else{
+      return(list(src=src, height="350"))
+    }
+    
+  },deleteFile = FALSE)
+  
+
+## Load the image
+
+  
+
+output$word<-renderWordcloud2({file1<-input$file
+
+
+
+image_path<-if(is.null(file1$datapath)){as.character(paste("www/",input$Image_class,sep=""))}else{file1$datapath}
+
+img<-image_load(image_path, target_size=c(224,224))
+
+
+x<-image_to_array(img)
+
+
+## Reshape the image
+
+x<-array_reshape(x,c(1,dim(x)))
+x<-if(input$Model == "resnet50"){
+  imagenet_preprocess_input(x)}
+else{inception_v3_preprocess_input(x)}
+
+## Make the prediction with imagenet pretrained model
+
+
+preds<-if(input$Model == "resnet50"){
+  model_resnet%>%predict(x)
+  }
+else{
+  model_inception%>%predict(x)
+}
+
+
+## Decode the label_text
+
+main = imagenet_decode_predictions(preds, top = 6)[[1]]
+text<-main$class_description
+
+
+## WordCloud
+
+Corpus=Corpus(VectorSource(text))
+Corpus=tm_map(Corpus,content_transformer(tolower))
+
+myDTM = TermDocumentMatrix(Corpus)
+m = as.matrix(myDTM)
+v = sort(rowSums(m), decreasing = TRUE)
+d<-data.frame(word=names(v),freq=v)
+## WordCloud
+
+wordcloud2(data = d,size=input$Font,color=as.character(paste(input$color)))
+
+
+})
+
+
+output$ggplot<-renderPlot({
+  file1<-input$file
+  image_path<-if(is.null(file1$datapath)){as.character(paste("www/",input$Image_class,sep=""))}else{file1$datapath}
+  img<-image_load(image_path, target_size=c(224,224))
+  x<-image_to_array(img)
+  
+  ## Reshape the image
+  
+  x<-array_reshape(x,c(1,dim(x)))
+  x<-if(input$Model == "resnet50"){
+    imagenet_preprocess_input(x)}
+  else{inception_v3_preprocess_input(x)}
+  
+  ## Make the prediction with imagenet pretrained model
+  preds<-if(input$Model == "resnet50"){
+    model_resnet%>%predict(x)
+  }
+  else{
+    model_inception%>%predict(x)
+  }
+  
+  
+  ## Decode the label_text
+  
+  main = imagenet_decode_predictions(preds, top = 6)[[1]]
+  
+  ## plot with ggplot2
+  par(bg="#F5EEF8")
+  ggplot(data=main,aes(x=reorder(class_description,score), y=score))+
+    geom_bar(stat = "identity", width = 0.5, fill="#F6862F")+
+    coord_flip()+
+    scale_color_brewer()+
+    ggtitle("Probable object with scores")+
+    xlab("Probable Objects")+
+    ylab("score")+
+    theme(panel.border = element_blank(), 
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.title = element_text(hjust = 0.5,size=18,colour="indianred4"),
+          axis.line = element_line(colour = "black"))+
+    theme(legend.position="none")
+  
+})
+
+}
+)
